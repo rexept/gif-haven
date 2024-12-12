158b7cc0e3fb9c6e692654cfa2c02ee8c8d35229
rexept <io@etriadi.com>
2024-12-12 14:32:55 -0700

    Added vitest

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	test: {
+		globals: true,
+		environment: 'jsdom',
+		include: ['**/*.test.jsx', '**/*.test.js'],
+		setupFiles: './setupTests.js', // Optional: To configure Jest-like matchers from jest-dom
+	},
 });
