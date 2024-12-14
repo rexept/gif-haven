0776ef576233e692a29bd5d02c6855fdabc5aa4d
rexept <io@etriadi.com>
2024-12-13 19:54:07 -0700

    Styles

--- src/pages/Stash/index.jsx
+++ src/pages/Stash/index.jsx
@@ -1,13 +1,18 @@
+import './Stash.css';
+
 import { IoArrowBackSharp } from 'react-icons/io5';
 import { GifList } from '../../components';
 import { useGifStorage } from '../../hooks/useGifStorage';
+import { NavLink } from 'react-router-dom';
 
 const Stash = () => {
 	const { gifs } = useGifStorage();
 
 	return (
-		<div data-testid='StashedGifs-container'>
-			<IoArrowBackSharp data-testid='StashedGifs-exit-button' className='stash-back-arrow' />
+		<div data-testid='StashedGifs-container' className='stash-container'>
+			<NavLink to='/'>
+				<IoArrowBackSharp data-testid='StashedGifs-exit-button' className='stash-back-arrow' />
+			</NavLink>
 			<GifList data-testid='StashedGifs-gifs-container' gifs={gifs} />
 		</div>
 	);
